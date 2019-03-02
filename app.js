038d42af117ed054bf1abeac2a8123388bb8bcaf
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-03-02 10:08:35 -0500

    Added classNames, ids to audio

--- app.js
+++ app.js
@@ -10,31 +10,31 @@ class App extends React.Component {
             <div id="drum-machine">
                 <div id="display">
                     <div className="drum-pad" id="Q">
-                        <audio src="" />
+                        <audio src="" className="clip" id="Q"/>
                     </div>
                     <div className="drum-pad" id="W">
-                        <audio src="" />
+                        <audio src="" className="clip" id="W"/>
                     </div>
                     <div className="drum-pad" id="E">
-                        <audio src="" />
+                        <audio src="" className="clip" id="E"/>
                     </div>
                     <div className="drum-pad" id="A">
-                        <audio src="" />                    
+                        <audio src="" className="clip" id="A"/>                    
                     </div>
                     <div className="drum-pad" id="S">
-                        <audio src="" />
+                        <audio src="" className="clip" id="S"/>
                     </div>
                     <div className="drum-pad" id="D">
-                        <audio src="" />
+                        <audio src="" className="clip" id="D"/>
                     </div>
                     <div className="drum-pad" id="Z">
-                        <audio src="" />
+                        <audio src="" className="clip" id="Z"/>
                     </div>
                     <div className="drum-pad" id="X">
-                        <audio src="" />
+                        <audio src="" className="clip" id="X"/>
                     </div>
                     <div className="drum-pad" id="C">
-                        <audio src="" />
+                        <audio src="" className="clip" id="C"/>
                     </div>
                 </div>
             </div>
