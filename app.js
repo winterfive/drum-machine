91823fd83a5b4074539a5a16612dea7b96038776
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-03-02 10:06:21 -0500

    Added classNmaes, ids

--- app.js
+++ app.js
@@ -9,15 +9,33 @@ class App extends React.Component {
         return(
             <div id="drum-machine">
                 <div id="display">
-                    <div className="drum-pad"></div>
-                    <div className="drum-pad"></div>
-                    <div className="drum-pad"></div>
-                    <div className="drum-pad"></div>
-                    <div className="drum-pad"></div>
-                    <div className="drum-pad"></div>
-                    <div className="drum-pad"></div>
-                    <div className="drum-pad"></div>
-                    <div className="drum-pad"></div>
+                    <div className="drum-pad" id="Q">
+                        <audio src="" />
+                    </div>
+                    <div className="drum-pad" id="W">
+                        <audio src="" />
+                    </div>
+                    <div className="drum-pad" id="E">
+                        <audio src="" />
+                    </div>
+                    <div className="drum-pad" id="A">
+                        <audio src="" />                    
+                    </div>
+                    <div className="drum-pad" id="S">
+                        <audio src="" />
+                    </div>
+                    <div className="drum-pad" id="D">
+                        <audio src="" />
+                    </div>
+                    <div className="drum-pad" id="Z">
+                        <audio src="" />
+                    </div>
+                    <div className="drum-pad" id="X">
+                        <audio src="" />
+                    </div>
+                    <div className="drum-pad" id="C">
+                        <audio src="" />
+                    </div>
                 </div>
             </div>
         )
