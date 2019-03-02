8287a2791994cc8c2f5cbcae0765fa174312e2fc
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-03-02 10:32:38 -0500

    Added title to audio sources

--- app.js
+++ app.js
@@ -8,39 +8,42 @@ class App extends React.Component {
     }
 
     handleClick() {
+        // TODO
         // onClick, play child div audio source
+        // pass in audio letter?
     }
 
+    // TODO find audio sources
     render() {
         return(
             <div id="drum-machine">
                 <div id="display">
                     <div className="drum-pad" id="Q" onClick={this.handleClick}>
-                        <audio src="" className="clip" id="Q"/>
+                        <audio src="" className="clip" id="Q" title=""/>
                     </div>
                     <div className="drum-pad" id="W" onClick={this.handleClick}>
-                        <audio src="" className="clip" id="W"/>
+                        <audio src="" className="clip" id="W" title=""/>
                     </div>
                     <div className="drum-pad" id="E" onClick={this.handleClick}>
-                        <audio src="" className="clip" id="E"/>
+                        <audio src="" className="clip" id="E" title="" />
                     </div>
                     <div className="drum-pad" id="A" onClick={this.handleClick}>
-                        <audio src="" className="clip" id="A"/>                    
+                        <audio src="" className="clip" id="A" title=""/>                    
                     </div>
                     <div className="drum-pad" id="S" onClick={this.handleClick}>
-                        <audio src="" className="clip" id="S"/>
+                        <audio src="" className="clip" id="S" title=""/>
                     </div>
                     <div className="drum-pad" id="D" onClick={this.handleClick}>
-                        <audio src="" className="clip" id="D"/>
+                        <audio src="" className="clip" id="D" title=""/>
                     </div>
                     <div className="drum-pad" id="Z" onClick={this.handleClick}>
-                        <audio src="" className="clip" id="Z"/>
+                        <audio src="" className="clip" id="Z" title=""/>
                     </div>
                     <div className="drum-pad" id="X" onClick={this.handleClick}>
-                        <audio src="" className="clip" id="X"/>
+                        <audio src="" className="clip" id="X" title=""/>
                     </div>
                     <div className="drum-pad" id="C" onClick={this.handleClick}>
-                        <audio src="" className="clip" id="C"/>
+                        <audio src="" className="clip" id="C" title=""/>
                     </div>
                 </div>
             </div>
