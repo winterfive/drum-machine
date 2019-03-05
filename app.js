c91c8cbe9363283e5da509b2f9fc79f016e2bc56
Lee <winterfive@users.noreply.github.com>
2019-03-05 12:02:18 -0500

    Made unique ids in json, corrected spelling errors

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const SOUNDS = [
-  {letter:"Q",id:"clunk",source:"https://www.dropbox.com/s/q5jjclpi4o4aspe/Garage%20Foley%20-%2028.wav?dl=1"},
-  {letter:"W",id:"clunk",source:"https://www.dropbox.com/s/ccdiydfiyo3n0wh/Garage%20Foley%20-%2029.wav?dl=1"},
-  {letter:"E",id:"clunk",source:"https://www.dropbox.com/s/gp2cm3bjj6r7syo/Garage%20Foley%20-%2030.wav?dl=1"},
-  {letter:"A",id:"clunk",source:"https://www.dropbox.com/s/va95q3ym0mw0kdp/Garage%20Foley%20-%2031.wav?dl=1"},
-  {letter:"S",id:"clunk",source:"https://www.dropbox.com/s/hmiyfh9rb290qzc/Garage%20Foley%20-%2032.wav?dl=1"},
-  {letter:"D",id:"clunk",source:"https://www.dropbox.com/s/5fvq9kk5wg9l20g/Garage%20Foley%20-%2033.wav?dl=1"},
-  {letter:"Z",id:"clunk",source:"https://www.dropbox.com/s/gsgcr8s0ctbp3mb/Garage%20Foley%20-%2034.wav?dl=1"},
-  {letter:"X",id:"clunk",source:"https://www.dropbox.com/s/u65rj99fnk1b2ot/Garage%20Foley%20-%2035.wav?dl=1"},
-  {letter:"C",id:"clunk",source:"https://www.dropbox.com/s/ua29jqxv4oazo4f/Garage%20Foley%20-%2071.wav?dl=1"}
-];
+  { letter:"Q", id:"clunk", source:"https://www.myinstants.com/media/sounds/snare.mp3"},
+  { letter:"W", id:"clunk2", source:"https://www.dropbox.com/s/ccdiydfiyo3n0wh/Garage%20Foley%20-%2029.wav"},
+  { letter:"E", id:"clunk3", source:"https://www.dropbox.com/s/gp2cm3bjj6r7syo/Garage%20Foley%20-%2030.wav"},
+  { letter:"A", id:"clunk4", source:"https://www.dropbox.com/s/va95q3ym0mw0kdp/Garage%20Foley%20-%2031.wav"},
+  { letter:"S", id:"clunk5", source:"https://www.dropbox.com/s/hmiyfh9rb290qzc/Garage%20Foley%20-%2032.wav"},
+  { letter:"D", id:"clunk6", source:"https://www.dropbox.com/s/5fvq9kk5wg9l20g/Garage%20Foley%20-%2033.wav"},
+  { letter:"Z", id:"clunk7", source:"https://www.dropbox.com/s/gsgcr8s0ctbp3mb/Garage%20Foley%20-%2034.wav"},
+  { letter:"X", id:"clunk8", source:"https://www.dropbox.com/s/u65rj99fnk1b2ot/Garage%20Foley%20-%2035.wav"},
+  { letter:"C", id:"clunk9", source:"https://www.dropbox.com/s/ua29jqxv4oazo4f/Garage%20Foley%20-%2071.wav"}
+]
 
 class Drum extends React.Component {
   
-  handleKeydown = e => {
+  componentDidMount() {
+    console.log("did mount")
+    document.addEventListener('keydown', this.handleKeyDown)
+    window.focus()
+  }  
+  
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+  
+  handleKeyDown = e => {
     if(e.keyCode === this.props.letter.charCodeAt()) {
       this.audio.play()
       this.audio.currentTime = 0
@@ -28,19 +38,19 @@ class Drum extends React.Component {
   
   render() {
     return(
-    <div 
-      className="drum-pad" 
-      id={this.props.id}
-      onCLick={this.handleClick}
+      <div 
+        className="drum-pad" 
+        id={this.props.id}
+        onClick={this.handleClick}
       >
         <h1>{this.props.letter}</h1>
         <audio
-          className="clip"
           id={this.props.letter}
+          className="clip"
           src={this.props.source}
-          ref={ref => this.audio = ref}>
-        </audio>             
-    </div>
+          ref={ref => this.audio = ref}
+        ></audio>             
+      </div>
     )
   }
 }
@@ -49,7 +59,7 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      display: "TODO"
+      display: "Click/Press key to play garage sound."
     }
   }
   
@@ -59,19 +69,20 @@ class App extends React.Component {
     return(
       <div id="drum-machine">
         <div id="display">{this.state.display}</div>
-        <div id="drums">
-          {SOUNDS.map(s => (
+        <div id="drums">{SOUNDS.map(s => (
             <Drum
+              key={s.id}
               id={s.id}
               letter={s.letter}
-              src={s.source}          
+              src={s.source}
+              handleDisplay={this.handleDisplay}
               />
           ))}
         </div>  
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
