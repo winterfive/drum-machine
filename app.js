8845a76c7d8f2bab1fe6ef8b7622b513ae2e3814
Lee <winterfive@users.noreply.github.com>
2019-03-05 15:31:26 -0500

    Corrected errors and added new soudn file links
    - Changed this.props.source to this.props.src

--- app.js
+++ app.js
@@ -1,88 +1,130 @@
 const SOUNDS = [
-  { letter:"Q", id:"clunk", source:"https://www.myinstants.com/media/sounds/snare.mp3"},
-  { letter:"W", id:"clunk2", source:"https://www.dropbox.com/s/ccdiydfiyo3n0wh/Garage%20Foley%20-%2029.wav"},
-  { letter:"E", id:"clunk3", source:"https://www.dropbox.com/s/gp2cm3bjj6r7syo/Garage%20Foley%20-%2030.wav"},
-  { letter:"A", id:"clunk4", source:"https://www.dropbox.com/s/va95q3ym0mw0kdp/Garage%20Foley%20-%2031.wav"},
-  { letter:"S", id:"clunk5", source:"https://www.dropbox.com/s/hmiyfh9rb290qzc/Garage%20Foley%20-%2032.wav"},
-  { letter:"D", id:"clunk6", source:"https://www.dropbox.com/s/5fvq9kk5wg9l20g/Garage%20Foley%20-%2033.wav"},
-  { letter:"Z", id:"clunk7", source:"https://www.dropbox.com/s/gsgcr8s0ctbp3mb/Garage%20Foley%20-%2034.wav"},
-  { letter:"X", id:"clunk8", source:"https://www.dropbox.com/s/u65rj99fnk1b2ot/Garage%20Foley%20-%2035.wav"},
-  { letter:"C", id:"clunk9", source:"https://www.dropbox.com/s/ua29jqxv4oazo4f/Garage%20Foley%20-%2071.wav"}
-]
+  {
+    letter: "Q",
+    id: "snare drum",
+    source: "https://www.myinstants.com/media/sounds/snare.mp3"
+  },
+  {
+    letter: "W",
+    id: "bongo",
+    source:
+      "http://tipiwiki.free.fr/snd/Percussion(4e)2.wav"
+  },
+  {
+    letter: "E",
+    id: "high hat",
+    source:
+      "http://www.denhaku.com/r_box/tr707/closed.wav"
+  },
+  {
+    letter: "A",
+    id: "bass",
+    source:
+      "https://www.myinstants.com/media/sounds/bass-drum.mp3"
+  },
+  {
+    letter: "S",
+    id: "tom-tom",
+    source:
+      "http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav"
+  },
+  {
+    letter: "D",
+    id: "low drum",
+    source:
+      "http://www.masterbits.de/sc_docu/sounds1/1TM00013.MP3"
+  },
+  {
+    letter: "Z",
+    id: "stick tap",
+    source:
+      "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+  },
+  {
+    letter: "X",
+    id: "clap",
+    source:
+      "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    letter: "C",
+    id: "kick",
+    source:
+      "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  }
+];
 
 class Drum extends React.Component {
-  
   componentDidMount() {
-    console.log("did mount")
-    document.addEventListener('keydown', this.handleKeyDown)
-    window.focus()
-  }  
-  
+    //console.log(this.audio)
+    document.addEventListener("keydown", this.handleKeydown);
+  }
+
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown)
+    document.removeEventListener("keydown", this.handleKeydown);
   }
-  
-  handleKeyDown = e => {
-    if(e.keyCode === this.props.letter.charCodeAt()) {
-      this.audio.play()
-      this.audio.currentTime = 0
-      this.props.handleDisplay(this.props.id)
+
+  // handle key press
+  handleKeydown = e => {
+    if (e.keyCode === this.props.letter.charCodeAt()) {
+      this.audio.play();
+      this.audio.currentTime = 0;
+      this.props.handleDisplay(this.props.id);
     }
-  }
-  
+  };
+
+
+  // handle mouse click
   handleClick = () => {
     this.audio.play();
     this.audio.currentTime = 0;
-    this.props.handleDisplay(this.props.id)
-  }
-  
+    this.props.handleDisplay(this.props.id);
+  };
+
   render() {
-    return(
-      <div 
-        className="drum-pad" 
-        id={this.props.id}
-        onClick={this.handleClick}
-      >
+    return (
+      <div className="drum-pad" id={this.props.id} onClick={this.handleClick}>
         <h1>{this.props.letter}</h1>
         <audio
           id={this.props.letter}
           className="clip"
-          src={this.props.source}
-          ref={ref => this.audio = ref}
-        ></audio>             
+          src={this.props.src}
+          ref={ref => (this.audio = ref)}>
+          </audio>
       </div>
-    )
+    );
   }
 }
 
+
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      display: "Click/Press key to play garage sound."
-    }
+      display: "Click/Press key to play a sound."
+    };
   }
-  
-  handleDisplay = display => this.setState({ display })
-  
+
+  handleDisplay = display => this.setState({ display });
+
   render() {
-    return(
+    return (
       <div id="drum-machine">
         <div id="display">{this.state.display}</div>
-        <div id="drums">{SOUNDS.map(s => (
+        <div id="drums">
+          {SOUNDS.map(s => (
             <Drum
               key={s.id}
               id={s.id}
               letter={s.letter}
               src={s.source}
               handleDisplay={this.handleDisplay}
-              />
+            />
           ))}
-        </div>  
+        </div>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
+ReactDOM.render(<App />, document.getElementById("root"));
